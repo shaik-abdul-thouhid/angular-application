585abdc1ab06abbd36e073cd697fa43b80d37159
Shaik Abdul Thouhid <shaikabdulthouhid.01@gmail.com>
2022-10-08 00:59:43 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-application';
+export class AppComponent implements OnInit {
+	public title: string = '';
+  	constructor() {}
+
+	ngOnInit(): void {
+		this.title = 'Hello There';
+	}
+
 }
